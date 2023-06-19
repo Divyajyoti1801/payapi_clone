29a5abe67d1efddedd5de4dbd6e4a2517dc4185d
Divyajyoti <31211823+Divyajyoti1801@users.noreply.github.com>
2023-06-19 14:15:18 +0530

    Pricing Page Completed

--- app/Pricing/page.tsx
+++ app/Pricing/page.tsx
@@ -1,7 +1,313 @@
-import React from "react";
+import Image from "next/image";
+import Container from "../components/Container";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Input from "../components/Input";
 
 const Pricing = () => {
-  return <div>Pricing</div>;
+  return (
+    <div className="relative w-[100%] flex flex-col items-center justify-between h-[100%] bg-[url('/images/shared/desktop/bg-pattern-circle.svg')] bg-no-repeat bg-auto bg-top lg:bg-right-top gap-20">
+      <Header />
+      <Container>
+        <h1 className="text-headingMedium w-full text-center font-serif text-sanJuanBlue font-semiBold">
+          Pricing
+        </h1>
+      </Container>
+      <Container className="flex flex-col md:flex-row gap-4 justify-center items-center">
+        <div className="flex flex-col gap-5 justify-center items-center">
+          <h1 className="text-customDarkPink text-headingSmall font-bold font-serif">
+            Free Plan
+          </h1>
+          <p className="hidden md:block text-center text-bodyText text-lightSanJuanBlue">
+            Build and test using our core set of products with up to 100 API
+            requests
+          </p>
+          <div className="w-full flex text-center items-center py-2 border-b-[2px] border-gray-200">
+            <h1 className="w-full text-center text-sanJuanBlue text-headingLarge font-serif font-bold">
+              $0.00
+            </h1>
+          </div>
+          <div className="w-full flex flex-col justify-center items-center border-b-[2px] border-gray-200 gap-5 pb-5">
+            <div className="w-[50%] flex flex-row justify-between items-center ">
+              <div className="w-[50%]">
+                <Image
+                  src="/images/shared/desktop/icon-check.svg"
+                  width={20}
+                  height={20}
+                  alt="Check"
+                />
+              </div>
+              <p className="w-[50%] text-bodyText text-sanJuanBlue font-semibold">
+                Transactions
+              </p>
+            </div>
+            <div className="w-[50%] flex flex-row justify-between items-center">
+              <div className="w-[50%]">
+                <Image
+                  src="/images/shared/desktop/icon-check.svg"
+                  width={20}
+                  height={20}
+                  alt="Check"
+                />
+              </div>
+              <p className="w-[50%] text-bodyText text-sanJuanBlue font-semibold">
+                Auth
+              </p>
+            </div>
+            <div className="w-[50%] flex flex-row justify-between items-center">
+              <div className="w-[50%]">
+                <Image
+                  src="/images/shared/desktop/icon-check.svg"
+                  width={20}
+                  height={20}
+                  alt="Check"
+                />
+              </div>
+              <p className="w-[50%] text-bodyText text-sanJuanBlue font-semibold">
+                Identity
+              </p>
+            </div>
+            <div className="w-[50%] flex flex-row justify-between items-center">
+              <div className="w-[50%]"></div>
+              <p className="w-[50%] text-bodyText text-lightSanJuanBlue font-semibold">
+                Investments
+              </p>
+            </div>
+            <div className="w-[50%] flex flex-row justify-between items-center">
+              <div className="w-[50%]"></div>
+              <p className="w-[50%] text-bodyText text-lightSanJuanBlue  font-semibold">
+                Assets
+              </p>
+            </div>
+            <div className="w-[50%] flex flex-row justify-between items-center">
+              <div className="w-[50%]"></div>
+              <p className="w-[50%] text-bodyText text-lightSanJuanBlue  font-semibold">
+                Liabilities
+              </p>
+            </div>
+            <div className="w-[50%] flex flex-row justify-between items-center">
+              <div className="w-[50%]"></div>
+              <p className="w-[50%] text-bodyText text-lightSanJuanBlue font-semibold">
+                Income
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5 justify-center items-center">
+          <h1 className="text-customDarkPink text-headingSmall font-bold font-serif">
+            Basic Plan
+          </h1>
+          <p className="hidden md:block text-center text-bodyText text-lightSanJuanBlue">
+            Launch your project with unlimited requests and no contractual
+            minimums
+          </p>
+          <div className="w-full flex text-center items-center py-2 border-b-[2px] border-gray-200">
+            <h1 className="w-full text-center text-sanJuanBlue text-headingLarge font-serif font-bold">
+              $249.00
+            </h1>
+          </div>
+          <div className="w-full flex flex-col justify-center items-center border-b-[2px] border-gray-200 gap-5 pb-5">
+            <div className="w-[50%] flex flex-row justify-between items-center ">
+              <div className="w-[50%]">
+                <Image
+                  src="/images/shared/desktop/icon-check.svg"
+                  width={20}
+                  height={20}
+                  alt="Check"
+                />
+              </div>
+              <p className="w-[50%] text-bodyText text-sanJuanBlue font-semibold">
+                Transactions
+              </p>
+            </div>
+            <div className="w-[50%] flex flex-row justify-between items-center">
+              <div className="w-[50%]">
+                <Image
+                  src="/images/shared/desktop/icon-check.svg"
+                  width={20}
+                  height={20}
+                  alt="Check"
+                />
+              </div>
+              <p className="w-[50%] text-bodyText text-sanJuanBlue font-semibold">
+                Auth
+              </p>
+            </div>
+            <div className="w-[50%] flex flex-row justify-between items-center">
+              <div className="w-[50%]">
+                <Image
+                  src="/images/shared/desktop/icon-check.svg"
+                  width={20}
+                  height={20}
+                  alt="Check"
+                />
+              </div>
+              <p className="w-[50%] text-bodyText text-sanJuanBlue font-semibold">
+                Identity
+              </p>
+            </div>
+            <div className="w-[50%] flex flex-row justify-between items-center">
+              <div className="w-[50%]">
+                <Image
+                  src="/images/shared/desktop/icon-check.svg"
+                  width={20}
+                  height={20}
+                  alt="Check"
+                />
+              </div>
+              <p className="w-[50%] text-bodyText text-sanJuanBlue font-semibold">
+                Investments
+              </p>
+            </div>
+            <div className="w-[50%] flex flex-row justify-between items-center">
+              <div className="w-[50%]">
+                <Image
+                  src="/images/shared/desktop/icon-check.svg"
+                  width={20}
+                  height={20}
+                  alt="Check"
+                />
+              </div>
+              <p className="w-[50%] text-bodyText text-sanJuanBlue  font-semibold">
+                Assets
+              </p>
+            </div>
+            <div className="w-[50%] flex flex-row justify-between items-center">
+              <div className="w-[50%]"></div>
+              <p className="w-[50%] text-bodyText text-lightSanJuanBlue  font-semibold">
+                Liabilities
+              </p>
+            </div>
+            <div className="w-[50%] flex flex-row justify-between items-center">
+              <div className="w-[50%]"></div>
+              <p className="w-[50%] text-bodyText text-lightSanJuanBlue font-semibold">
+                Income
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5 justify-center items-center">
+          <h1 className="text-customDarkPink text-headingSmall font-bold font-serif">
+            Premium Plan
+          </h1>
+          <p className="hidden md:block text-center text-bodyText text-lightSanJuanBlue">
+            Get tailored solutions, volume pricing, and dedicated support for
+            your team
+          </p>
+          <div className="w-full flex text-center items-center py-2 border-b-[2px] border-gray-200">
+            <h1 className="w-full text-center text-sanJuanBlue text-headingLarge font-serif font-bold">
+              $499.00
+            </h1>
+          </div>
+          <div className="w-full flex flex-col justify-center items-center border-b-[2px] border-gray-200 gap-5 pb-5">
+            <div className="w-[50%] flex flex-row justify-between items-center ">
+              <div className="w-[50%]">
+                <Image
+                  src="/images/shared/desktop/icon-check.svg"
+                  width={20}
+                  height={20}
+                  alt="Check"
+                />
+              </div>
+              <p className="w-[50%] text-bodyText text-sanJuanBlue font-semibold">
+                Transactions
+              </p>
+            </div>
+            <div className="w-[50%] flex flex-row justify-between items-center">
+              <div className="w-[50%]">
+                <Image
+                  src="/images/shared/desktop/icon-check.svg"
+                  width={20}
+                  height={20}
+                  alt="Check"
+                />
+              </div>
+              <p className="w-[50%] text-bodyText text-sanJuanBlue font-semibold">
+                Auth
+              </p>
+            </div>
+            <div className="w-[50%] flex flex-row justify-between items-center">
+              <div className="w-[50%]">
+                <Image
+                  src="/images/shared/desktop/icon-check.svg"
+                  width={20}
+                  height={20}
+                  alt="Check"
+                />
+              </div>
+              <p className="w-[50%] text-bodyText text-sanJuanBlue font-semibold">
+                Identity
+              </p>
+            </div>
+            <div className="w-[50%] flex flex-row justify-between items-center">
+              <div className="w-[50%]">
+                <Image
+                  src="/images/shared/desktop/icon-check.svg"
+                  width={20}
+                  height={20}
+                  alt="Check"
+                />
+              </div>
+              <p className="w-[50%] text-bodyText text-sanJuanBlue font-semibold">
+                Investments
+              </p>
+            </div>
+            <div className="w-[50%] flex flex-row justify-between items-center">
+              <div className="w-[50%]">
+                <Image
+                  src="/images/shared/desktop/icon-check.svg"
+                  width={20}
+                  height={20}
+                  alt="Check"
+                />
+              </div>
+              <p className="w-[50%] text-bodyText text-sanJuanBlue  font-semibold">
+                Assets
+              </p>
+            </div>
+            <div className="w-[50%] flex flex-row justify-between items-center">
+              <div className="w-[50%]">
+                <Image
+                  src="/images/shared/desktop/icon-check.svg"
+                  width={20}
+                  height={20}
+                  alt="Check"
+                />
+              </div>
+              <p className="w-[50%] text-bodyText text-sanJuanBlue  font-semibold">
+                Liabilities
+              </p>
+            </div>
+            <div className="w-[50%] flex flex-row justify-between items-center">
+              <div className="w-[50%]">
+                <Image
+                  src="/images/shared/desktop/icon-check.svg"
+                  width={20}
+                  height={20}
+                  alt="Check"
+                />
+              </div>
+              <p className="w-[50%] text-bodyText text-sanJuanBlue font-semibold">
+                Income
+              </p>
+            </div>
+          </div>
+        </div>
+      </Container>
+      <Container>
+        <div className="w-full flex flex-col lg:flex-row justify-center lg:justify-between items-center gap-5">
+          <h1 className="w-full text-center lg:text-left lg:w-[50%] text-headingMedium text-sanJuanBlue font-serif font-bold">
+            Ready to start?
+          </h1>
+          <div className="w-full lg:w-[50%] flex justify-center lg:justify-end items-center">
+            <Input prompt={false} className="w-full" />
+          </div>
+        </div>
+      </Container>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default Pricing;
